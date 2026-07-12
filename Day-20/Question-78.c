1389eca69dceb5bd15308becb9544d07e37d7343
Srijan Pandey <srijan.pandey2025@glbajajgroup.org>
2026-07-12 18:52:26 +0530

    Day 20 uploaded

---
+++ Day-20/Question-78.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a[3][3]={{1,2,3},{2,1,3},{3,2,1}},i,j,flag=1;
+    for(i=0;i<3;i++)
+        for(j=0;j<3;j++)
+            if(a[i][j]!=a[j][i]) flag=0;
+    if(flag) printf("Symmetric"); else printf("Not Symmetric");
+}

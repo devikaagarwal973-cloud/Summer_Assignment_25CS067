e507192f0083638893bc0dae22d5c26e22558223
Srijan Pandey <srijan.pandey2025@glbajajgroup.org>
2026-07-12 18:50:48 +0530

    Day 18 uploaded

---
+++ Day-18/Question-69.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a[5]={5,3,4,1,2},i,j,temp;
+    for(i=0;i<5-1;i++)
+        for(j=0;j<5-i-1;j++)
+            if(a[j]>a[j+1]){temp=a[j];a[j]=a[j+1];a[j+1]=temp;}
+    for(i=0;i<5;i++) printf("%d ",a[i]);
+}
---
+++ Day-18/Question-70.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a[5]={5,3,4,1,2},i,j,min,temp;
+    for(i=0;i<4;i++){
+        min=i;
+        for(j=i+1;j<5;j++) if(a[j]<a[min]) min=j;
+        temp=a[i];a[i]=a[min];a[min]=temp;
+    }
+    for(i=0;i<5;i++) printf("%d ",a[i]);
+}
